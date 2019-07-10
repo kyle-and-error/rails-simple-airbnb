36567a81b21de7f09eec6c3674530ff5467b0db4
Kyle Cho <kylescho99@gmail.com>
2019-07-10 16:10:30 +0200

    Views

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Cleaning database...'
-Flats.destroy_all
+Flat.destroy_all
 
 puts 'Creating restaurants...'
 flats_attributes = [
